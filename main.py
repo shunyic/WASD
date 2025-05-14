39f0539bfc002aa5d6bec9e54eb54c84d14af4f2
legoat <144399666+shunyic@users.noreply.github.com>
2025-05-14 14:47:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,24 @@ def get_student_schedule(student_id):
   statement = f"call FindSchedule({student_id});"
   return execute_statement(get_database_connection("shunyic", "231084559", "10.8.37.226", "shunyic_db"), statement)
 
+def print_student_schedule(id):
+    results = get_student_schedule(id)
 
+    for result in results:
+        print(f"Period: {result[0]} \nCourse: {result[1]} \nRoom: {result[2]} \nTeacher: {result[3]}\n")
+
+def get_teacher_schedule(teacher_id):
+    statement = f"call TeacherSchedule({teacher_id});"
+    return execute_statement(get_database_connection("shunyic", "231084559", "10.8.37.226", "shunyic_db"), statement)
+
+def print_teacher_schedule(id):
+    results = get_teacher_schedule(id)
+    for result in results:
+        print(f"Period: {result[1]} \nCourse: {result[0]} \nRoom: {result[2]} \n")
+
+job = input("Teacher or Student: ")
 id = input("ID: ")
-results = get_student_schedule(id)
-
-for result in results:
-  print(f"Period: {result[0]} \nCourse: {result[1]} \nRoom: {result[2]} \nTeacher: {result[3]}\n")
+if job == "Teacher":
+    print_teacher_schedule(id)
+else:
+    print_student_schedule(id)
